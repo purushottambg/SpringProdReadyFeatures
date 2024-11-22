0ec56b934acc3ccdbe8cfa7e88abfc18548da740
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2024-11-22 23:40:49 +0530

    Project .zip file Successfully Extracted

---
+++ src/main/java/com/prod_ready_features/prodready/ProdreadyApplication.java
@@ -0,0 +1,13 @@
+package com.prod_ready_features.prodready;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdreadyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdreadyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prod_ready_features/prodready/ProdreadyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prod_ready_features.prodready;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdreadyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
